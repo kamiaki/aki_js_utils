63892f2767c14a8913c2d8efe5036f947499c2ae
kamiaki <247557768@qq.com>
2021-01-29 01:06:17 +0800

    dateformat

--- test.js
+++ test.js
@@ -1,8 +1,4 @@
 let sdtime1 = new Date('2018-03-22 16:34:55')
-let daySart = new Date(new Date(new Date().toLocaleDateString()).getTime())
-let dayEnd = new Date(new Date(new Date().toLocaleDateString()).getTime() + 24 * 60 * 60 * 1000 - 1)
-console.info(daySart.toLocaleString())
-console.info(dayEnd.toLocaleString())
 
 const floorTo10Minutes = (date) => {
     date.setMinutes(date.getMinutes() - date.getMinutes() % 10)
@@ -12,22 +8,24 @@ const floorTo10Minutes = (date) => {
 }
 
 const changeDate = (date, type, number) => {
+    let newDate = new Date(date)
     if ('yyyy' === type) {
-        date.setFullYear(date.getFullYear() + number)
+        newDate.setFullYear(newDate.getFullYear() + number)
     } else if ('MM' === type) {
-        date.setMonth(date.getMonth() + number)
+        newDate.setMonth(newDate.getMonth() + number)
     } else if ('dd' === type) {
-        date.setDate(date.getDate() + number)
+        newDate.setDate(newDate.getDate() + number)
     } else if ('HH' === type) {
-        date.setHours(date.getHours() + number)
+        newDate.setHours(newDate.getHours() + number)
     } else if ('mm' === type) {
-        date.setMinutes(date.getMinutes() + number)
+        newDate.setMinutes(newDate.getMinutes() + number)
     } else if ('ss' === type) {
-        date.setSeconds(date.getSeconds() + number)
+        newDate.setSeconds(newDate.getSeconds() + number)
     } else if ('S' === type) {
-        date.setMilliseconds(date.getMilliseconds() + number)
+        newDate.setMilliseconds(newDate.getMilliseconds() + number)
     }
+    return newDate
 }
 floorTo10Minutes(sdtime1)
-changeDate(sdtime1, 'dd', -23)
-console.info(sdtime1.toLocaleString())
+let a = changeDate(sdtime1, 'MM', 10)
+console.info(a.toLocaleString())
